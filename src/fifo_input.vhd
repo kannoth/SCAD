3c8f291dcefadde2c55ec91cbac74acb0d1b631e
Mahircan Gul <mahircg@gmail.com>
2015-11-30 11:33:21 +0100

    Use single process to avoid timing anomalies

--- src/fifo_input.vhd
+++ src/fifo_input.vhd
@@ -30,7 +30,7 @@ type 	fsm_type is (reset,wait_cmd,write_buffer,read_buffer);
 type 	fifo_buffer_type is array (0 to BUF_SIZE-1) of STD_LOGIC_VECTOR (DATA_WIDTH-1 downto 0);	
 
 signal 	fifo_buffer : fifo_buffer_type;													
-signal	ps,ns : fsm_type;
+signal	state : fsm_type;
 
 
 signal 	head : natural range 0 to BUF_SIZE - 1 ;									
@@ -42,6 +42,7 @@ signal 	reg_empty : std_logic ;
 
 begin
 
+
 full 	<= reg_full;
 empty <= reg_empty;
 
@@ -49,90 +50,83 @@ sequential_proc: process(clk)
 begin
 	if rising_edge(clk) then
 		if rst = '1' then
-			ps <= reset;
+			state <= reset;
 		else
-			ps <= ns;
+			case state is
+			when reset => 
+				head <= 0;
+				tail <= 0;
+				reg_full <= '0';
+				reg_empty <= '1';
+				num_elements <= 0;
+				if en = '1' then 
+					state <= wait_cmd;
+				else
+					state <= reset;
+				end if;
+			when wait_cmd =>
+				if en = '1' then	
+					if rw = '0' then
+						if reg_empty = '0' then
+							data_out <= fifo_buffer(head);
+							num_elements <= num_elements - 1;
+							state <= read_buffer;
+						else
+							data_out <= (others => 'Z');
+							num_elements <= num_elements;
+							state <= wait_cmd;
+						end if;
+					else
+						if reg_full = '0' then 
+							fifo_buffer(tail) <= data_in;
+							num_elements <= num_elements + 1;
+							state <= write_buffer;
+						else
+							fifo_buffer(tail) <= fifo_buffer(tail);
+							num_elements <= num_elements;
+							state <= wait_cmd;
+						end if;
+						data_out <= (others => 'Z');
+					end if;
+				else
+					data_out <= (others => 'Z');
+					state <= wait_cmd;
+				end if;
+			when read_buffer =>
+				if num_elements /= 0 then
+					if head /= BUF_SIZE -1 then
+						head <= head + 1 ;
+					else
+						head <= 0;
+					end if;
+					reg_empty <= '0';
+				else
+					reg_empty <= '1';
+				end if;
+				reg_full <= '0';
+				state <= wait_cmd;
+				
+			when write_buffer => 
+				if num_elements = BUF_SIZE then
+					reg_full <= '1';
+				else
+					reg_full <= '0';
+				end if;
+				
+				if tail /= BUF_SIZE -1 then
+					tail <= tail + 1 ;
+				else
+					tail <= 0;
+				end if;
+				
+				reg_empty <= '0';
+				state <= wait_cmd;
+			end case;
 		end if;
 	end if;
 end process;
 
 
-
-comb_process: process(ps,rw,en)	
-			
-begin
-case ps is
-	when reset => 
-		head <= 0;
-		tail <= 0;
-		reg_full <= '0';
-		reg_empty <= '1';
-		num_elements <= 0;
-		if en = '1' then 
-			ns <= wait_cmd;
-		else
-			ns <= reset;
-		end if;
-	when wait_cmd =>
-		if en = '1' then	
-			if rw = '0' then
-				if reg_empty = '0' then
-					data_out <= fifo_buffer(head);
-					num_elements <= num_elements - 1;
-					ns <= read_buffer;
-				else
-					data_out <= (others => 'Z');
-					num_elements <= num_elements;
-					ns <= wait_cmd;
-				end if;
-			else
-				if reg_full = '0' then 
-					fifo_buffer(tail) <= data_in;
-					num_elements <= num_elements + 1;
-					ns <= write_buffer;
-				else
-					fifo_buffer(tail) <= fifo_buffer(tail);
-					num_elements <= num_elements;
-					ns <= wait_cmd;
-				end if;
-				data_out <= (others => 'Z');
-			end if;
-		else
-			data_out <= (others => 'Z');
-			ns <= wait_cmd;
-		end if;
-	when read_buffer =>
-		if num_elements /= 0 then
-			if head /= BUF_SIZE -1 then
-				head <= head + 1 ;
-			else
-				head <= 0;
-			end if;
-			reg_empty <= '0';
-		else
-			reg_empty <= '1';
-		end if;
-		reg_full <= '0';
-		ns <= wait_cmd;
-		
-	when write_buffer => 
-		if num_elements = BUF_SIZE then
-			reg_full <= '1';
-		else
-			reg_full <= '0';
-		end if;
-		
-		if tail /= BUF_SIZE -1 then
-			tail <= tail + 1 ;
-		else
-			tail <= 0;
-		end if;
-		
-		reg_empty <= '0';
-		ns <= wait_cmd;
-	end case;
-	
-end process;
 
 
 end Behavioral;
