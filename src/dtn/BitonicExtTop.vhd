2b035ad3df1d91791627e816516265a96b6fde69
Subash Kannoth <kannoth@localhost.localdomain>
2016-01-25 01:10:04 +0100

    Add: 1) Extra wrapper "BitonicAddressMux" module to sort out inclomplete inputs to the bitonic sorter. 2) The addressbus size increased from 5 to 6 bit inorder to handle in invalid addresses. 3) Introduced validity bit "val" fro asserting the valdity fo the data routed to FUs. 3) Testbench for the above feature.

---
+++ src/dtn/BitonicExtTop.vhd
@@ -0,0 +1,19 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+library work;
+use work.glbSharedTypes.all;
+
+entity BitonicExtTop is 
+	port (
+    		inp_vector : in  bitonStageBus_t;
+    		out_vector : out bitonStageBus_t		
+	);
+end entity;
+
+architecture Behaviour of BitonicExtTop is
+	signal wire  : bitonStageBus_t;
+begin
+	bitonAddrMuxInst : entity work.BitonicAddressMux(Behaviour) port map(inp_vector => inp_vector, out_vector => wire			);
+	bitonNetworkInst : entity work.BitonicTop(Behaviour) 			port map(inp_vector => wire 		, out_vector => out_vector	);
+end architecture;
