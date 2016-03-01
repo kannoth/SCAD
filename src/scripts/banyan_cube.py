4a14cd2e41975c2f59ad86e61ff27171fbba2f20
Mahircan Gul <mahircg@gmail.com>
2016-03-01 19:26:01 +0100

    Add pipelined network generation option

--- src/scripts/banyan_cube.py
+++ src/scripts/banyan_cube.py
@@ -1,15 +1,9 @@
-
-
 class SE():
 	def __init__(self, inp_a, inp_b, out_a, out_b):
 		self.inp_a = inp_a
 		self.inp_b = inp_b
 		self.out_a = out_a
 		self.out_b = out_b
-	def __repr__(self):
-		return "I1:%s, I2:%s" % (self.inp_a, self.inp_b)
-
-
 
 def gen_network(nr_of_stages):
 	stage_list = []
@@ -27,7 +21,7 @@ def gen_network(nr_of_stages):
 		stage_list.append(switch_list)
 	return stage_list
 	
-def gen_vhdl(nr_of_stages):
+def gen_vhdl(nr_of_stages,is_pipelined):
 	
 	cmd_list=[]
 	num_elements = pow(2,nr_of_stages)
@@ -39,30 +33,51 @@ def gen_vhdl(nr_of_stages):
 	cmd_list.append("\tport (\n\t\tclk 	: in std_logic;\n\t\treset	: in std_logic;\n"
 	"\t\tin_data_packets	: in data_packets_t;\n"
 	"\t\tout_data_packets	: out data_packets_t);\nend entity;")
-	
 	cmd_list.append("architecture RTL of BanyanNetwork is\n\n")
 	cmd_list.append("--wiring signals")
-	for j in range (0,num_elements):
-		cmd_list.append("signal stage%s_sig%s : data_port_sending;" % (0,j))
-	for i in range(1,nr_of_stages+1):
-		for k in range (0,num_elements):
-			cmd_list.append("signal stage%s_sig%s : data_port_sending;" % (i,k))
+	for i in range(0,nr_of_stages):
+		cmd_list.append("signal stage%s_sig : data_packets_t;" % i)
+	if is_pipelined == True:
+		for i in range(0,nr_of_stages):
+			cmd_list.append("signal stage%s_reg : data_packets_t;" % i)
 	cmd_list.append("\n\nbegin\n\n")
 	cmd_list.append("--Input,output port assignments\n\n")
-	for j in range (0,num_elements):
-		cmd_list.append("stage0_sig%s \t<= in_data_packets(%s);" % (j,j))
-	for j in range (0,num_elements):
-		cmd_list.append("out_data_packets(%s) \t<= stage%s_sig%s;" % (j,nr_of_stages,j))
+	if is_pipelined == False:
+		cmd_list.append("stage0_sig \t<= in_data_packets;")
 	cmd_list.append("\n\n")
-	for i in range(0,nr_of_stages):
+	src = "reg" if is_pipelined == True else "sig";
+	for i in range(0,nr_of_stages-1):
 		inp_base = "stage%s" % i
-		out_base = "stage%s" % (i+1)
+		out_base = "stage%s" % (i+1) if i != nr_of_stages-1 else "out_data_packets";
 		for j in range(0, num_elements/2):
 			cmd_list.append("STAGE%s_SWITCH%s : entity work.BanyanSwitch(RTL)\n\tgeneric map(idx => %s)\n\t port map(" % (i,j,nr_of_stages-1-i))
-			cmd_list.append("\t%s_sig%s,%s_sig%s"			
-			",%s_sig%s,%s_sig%s);\n" % (inp_base,stages[i][j].inp_a,
-			inp_base,stages[i][j].inp_b,out_base,stages[i][j].out_a,out_base,
+			cmd_list.append("\t%s_%s(%s),%s_%s(%s)"			
+			",%s_sig(%s),%s_sig(%s));\n" % (inp_base,src,stages[i][j].inp_a,
+			inp_base,src,stages[i][j].inp_b,out_base,stages[i][j].out_a,out_base,
 			stages[i][j].out_b))
+	inp_base = "stage%s" % (nr_of_stages-1)
+	out_base = "out_data_packets";
+	for j in range(0, num_elements/2):
+			cmd_list.append("STAGE%s_SWITCH%s : entity work.BanyanSwitch(RTL)\n\tgeneric map(idx => %s)\n\t port map(" % (nr_of_stages-1,j,1))
+			cmd_list.append("\t%s_%s(%s),%s_%s(%s)"			
+			",%s(%s),%s(%s));\n" % (inp_base,src,stages[nr_of_stages-1][j].inp_a,
+			inp_base,src,stages[nr_of_stages-1][j].inp_b,out_base,stages[nr_of_stages-1][j].out_a,out_base,
+			stages[nr_of_stages-1][j].out_b))	
+	
+	cmd_list.append("\n\n")
+	if is_pipelined == True:
+		cmd_list.append("pipe_proc: process(clk)\n\tbegin")
+		cmd_list.append("\t\tif rising_edge(clk) then")
+		cmd_list.append("\t\t\tif reset = '1' then")
+		cmd_list.append("\t\t\t\tnull;")
+		cmd_list.append("\t\t\telse")
+		cmd_list.append("\t\t\t\tstage0_reg \t<= in_data_packets;")
+		for i in range(1,nr_of_stages):
+			cmd_list.append("\t\t\t\tstage%s_reg \t<= stage%s_sig;" % (i,i))
+		cmd_list.append("\t\t\tend if;")
+		cmd_list.append("\t\tend if;")
+		cmd_list.append("\tend process;")
+		
 	cmd_list.append("\n\nend architecture;\n\n")
 	file = open("banyan_cube.vhdl",'w')
 	for l in cmd_list:
