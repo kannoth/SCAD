8af965caacb803038466c3f5eaed9a70606680ac
Subash Kannoth <kannoth@localhost.localdomain>
2016-03-06 20:42:39 +0100

    Modify: New modified test case for Bitonic+Banyan Cube

--- sim/BitonicAdapterTB.vhd
+++ sim/BitonicAdapterTB.vhd
@@ -8,7 +8,7 @@ entity BitonicAdapterTB is
 end entity;
 
 architecture Behaviour of BitonicAdapterTB is
-	signal	clk_period_s		: time := 40ns;
+	signal	clk_period_s		: time := 40 ns;
 	signal	clk_s			: std_logic := '0';
 	signal	reset_s			: std_logic := '0';
 	signal	in_data_packets_s	: data_packets_t;
@@ -21,14 +21,17 @@ architecture Behaviour of BitonicAdapterTB is
 	  constant  vld_vector_1 : validVector_t    := ('1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1',
 															'1','1','1','1','1','1','1','1','1');
 	-- Randomn case : some inputs are valid
-	  constant  des_addr_2 : testVector_t := (0,1,4,2,29,3,7,8,9,28,10,30,11,20,24,12,13,26,14,15,17,18,19,21,22,23,25,27,31,5,6,16);
-	  constant  src_addr_2 : testVector_t := (0,1,4,2,29,3,7,8,9,28,10,30,11,20,24,12,13,26,14,15,17,18,19,21,22,23,25,27,31,5,6,16);
+	  --constant  des_addr_2 : testVector_t := (0,1,4,2,29,3,7,8,9,28,10,30,11,20,24,12,13,26,14,15,17,18,19,21,22,23,25,27,31,5,6,16);
+	  --constant  src_addr_2 : testVector_t := (0,1,4,2,29,3,7,8,9,28,10,30,11,20,24,12,13,26,14,15,17,18,19,21,22,23,25,27,31,5,6,16);
+	  -- 0,1,4,2,16 are INVALID addresses
+	  constant  des_addr_2 : testVector_t := (15,15,15,15,29,3,7,8,9,28,10,30,11,20,24,12,13,26,14,15,17,18,19,21,22,23,25,27,31,5,6,17);
+	  constant  src_addr_2 : testVector_t := (15,15,15,15,29,3,7,8,9,28,10,30,11,20,24,12,13,26,14,15,17,18,19,21,22,23,25,27,31,5,6,17);
 	  constant  vld_vector_2 : validVector_t    := ('0','0','0','0','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1',
 															'1','1','1','1','1','1','1','1','0');
 	-- Worst case : all inputs are invalid
 	  constant  des_addr_3 : testVector_t := (4,1,16,2,29,3,7,8,9,28,10,30,11,20,24,12,13,26,14,15,17,18,19,21,22,23,25,27,31,5,6,0);
 	  constant  src_addr_3 : testVector_t := (4,1,16,2,29,3,7,8,9,28,10,30,11,20,24,12,13,26,14,15,17,18,19,21,22,23,25,27,31,5,6,0);
-	  constant  vld_vector_3 : validVector_t    := ('1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0',
+	  constant  vld_vector_3 : validVector_t    := ('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0',
 															'0','0','0','0','0','0','0','0','0');
 begin
 	BitonicAdapter_Inst : entity work.BitonicAdapter(Behaviour) port
@@ -62,9 +65,9 @@ begin
 		wait for 500 ns;
 			reset_s <= '0';
 		for i in 0 to 31 loop
-			in_data_packets_s(i).valid		<= vld_vector_3(i);
-			in_data_packets_s(i).message.dest.fu 	<= std_logic_vector(to_unsigned(des_addr_3(i),5));
-			in_data_packets_s(i).message.src.fu 	<= std_logic_vector(to_unsigned(src_addr_3(i),5));
+			in_data_packets_s(i).valid		<= vld_vector_2(i);
+			in_data_packets_s(i).message.dest.fu 	<= std_logic_vector(to_unsigned(des_addr_2(i),5));
+			in_data_packets_s(i).message.src.fu 	<= std_logic_vector(to_unsigned(src_addr_2(i),5));
 			in_data_packets_s(i).message.data	<= std_logic_vector(to_unsigned(0,32));
 			in_data_packets_s(i).message.dest.buff 	<='1';
 			in_data_packets_s(i).message.src.buff 	<='1';
